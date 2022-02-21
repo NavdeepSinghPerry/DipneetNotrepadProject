d561065bfc0847117e37067d6fa8c3434649bbc4
Navdeep Singh <navdeepsingh@microsoft.com>
2022-02-21 20:47:33 +0530

    New

---
+++ DemoConsoleApp/DemoConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsoleApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int x = 5;
+			Console.WriteLine(x);
+			Console.WriteLine(x);
+			Console.Read();
+		}
+	}
+}
